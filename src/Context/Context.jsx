913602075c305cfa28370ef9a54d4ce84d33c073
pasindud100 <dilshanp5400@gmail.com>
2024-11-09 23:45:27 -0800

    create new chat and get recently generated content functions

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -10,32 +10,46 @@ const ContextProvider = (props) => {
   const [showResult, setShowResult] = useState(false); // if this is true this hide the all the cars in the main content and show prmpted resut there
   const [loading, setLoading] = useState(false); // when this tru the loading animation apere.. this will false when prompted resut come
   const [resultData, setResultData] = useState(""); // this is to show  result
- 
- 
-  const delaypara = (index, nextWord) => { 
+
+  const delaypara = (index, nextWord) => {
     setTimeout(function () {
       setResultData((prev) => prev + nextWord);
     }, 75 * index);
+  };
+
+  const newChat = () => {
+    // to crete new chat when click new cht icon
+    setShowResult(false);
+    setLoading(false);
   };
 
   const onSent = async (prompt) => {
     setResultData("");
     setLoading(true);
     setShowResult(true);
-    setRecentPrompt(input);
-    setPrePrompts((prev) => [...prev, input]); // this is to store the previous prompts
 
-    const response = await runChat(input);
+    let response;
+    if (prompt !== undefined) {
+      response = await runChat(prompt);
+      setRecentPrompt(prompt);
+    } else {
+      setPrePrompts((prev) => [...prev, input]);
+      setRecentPrompt(prompt);
+      response = await runChat(input);
+    }
 
     let responseArray = response.split("**");
-    let newResponse="";//we use "" to prevent show the first word in result as undefined
-    for (let i = 0; i < responseArray.length; i++) { //emove ** and convert it ro blod words
-      if (i === 0 || i % 2 !== 1) { //even number
+    let newResponse = ""; //we use "" to prevent show the first word in result as undefined
+    for (let i = 0; i < responseArray.length; i++) {
+      //emove ** and convert it ro blod words
+      if (i === 0 || i % 2 !== 1) {
+        //even number
         newResponse += responseArray[i];
       } else {
         newResponse += "<b>" + responseArray[i] + "</b>";
       }
     }
+
     let newResponse2 = newResponse.split("*").join("</br>");
 
     let newResponseArray = newResponse2.split(" ");
@@ -60,6 +74,7 @@ const ContextProvider = (props) => {
     resultData,
     input,
     setInput,
+    newChat,
   };
 
   return (
--- src/components/slideBar/SideBar.jsx
+++ src/components/slideBar/SideBar.jsx
@@ -5,7 +5,13 @@ import { Context } from "../../Context/Context";
 
 function SlidBar() {
   const [extended, setExtended] = useState(false); //to show the extended side bar
-  const { onSent, prePrompts, setRecentPrompts } = useContext(Context);
+  const { onSent, prePrompts, setRecentPrompt, newChat } = useContext(Context);
+
+  //loardprompt function for loard the result again when clik on of recent promt from slide bara
+  const loadPrompt = async (prompt) => {
+    setRecentPrompt(prompt)
+    await onSent(prompt);
+  };
 
   return (
     <div className="slidebar">
@@ -16,7 +22,7 @@ function SlidBar() {
           className="menu"
           alt=""
         />
-        <div className="new-chat">
+        <div className="new-chat" onClick={()=>newChat()}>
           <img src={assets.plus_icon} alt="" />
           {extended ? <p>New Chat</p> : null}
         </div>
@@ -26,10 +32,10 @@ function SlidBar() {
             <p className="recentTitle"> Recent</p>
             {prePrompts.map((item, index) => {
               return (
-                <div className="recent-entry">
+                <div className="recent-entry" onClick={()=> loadPrompt(item)}>
                   <img src={assets.message_icon} alt="" />
                   {/* //slice to show only 15 characters in sliodebar recent promt */}
-                  <p>{item.slice(0, 15)}</p> 
+                  <p>{item.slice(0, 15)}</p>
                 </div>
               );
             })}
