0081636cb626a06d9277af34909cc08e99f702fa
pasindud100 <dilshanp5400@gmail.com>
2024-11-09 18:16:44 -0800

    Make slidebar modifications.

--- src/components/slideBar/SideBar.jsx
+++ src/components/slideBar/SideBar.jsx
@@ -1,42 +1,45 @@
-import React from 'react';
-import './SideBar.css';
-import { assets} from '../../assets/assets';
+import React, { useState } from "react";
+import "./SideBar.css";
+import { assets } from "../../assets/assets";
 
 function SlidBar() {
+  const [extended, setExtended] = useState(false); //to show the extended side bar
+
   return (
-    <div className='slidebar'>
+    <div className="slidebar">
       <div className="top">
-          <img src={assets.menu_icon} className='menu' alt="" />
-          <div className="new-chat">
-            <img src={assets.plus_icon} alt="" />
-            <p>New Chat</p>
+        <img onClick={() => setExtended(pre=>!pre)} src={assets.menu_icon} className="menu" alt="" />
+        <div className="new-chat">
+          <img src={assets.plus_icon} alt="" />
+          {extended ? <p>New Chat</p> : null}
+        </div>
+        
+        {extended ? <div className="recent">
+          <p className="recentTitle"> Recent</p>
+          <div className="recent-entry">
+            <img src={assets.message_icon} alt="" />
+            <p>What is ...</p>
           </div>
-          <div className="recent">
-            <p className='recentTitle'> Recent</p>
-            <div className="recent-entry">
-              <img src={assets.message_icon} alt="" />
-              <p>What is HTML ...</p>
-            </div>
-          </div>
+        </div> : null}
       </div>
       <div className="bottom">
-          <div className="bottom_item recent-entry">
-            <img src={assets.question_icon} alt="" />
-            <p>Help</p>
-          </div>
+        <div className="bottom_item recent-entry">
+          <img src={assets.question_icon} alt="" />
+          {extended ? <p>Help</p> : null}
+        </div>
 
-          <div className="bottom_item recent-entry">
-            <img src={assets.history_icon} alt="" />
-            <p>Activities</p>
-          </div>
+        <div className="bottom_item recent-entry">
+          <img src={assets.history_icon} alt="" />
+     {extended?      <p>Activities</p>: null}
+        </div>
 
-          <div className="bottom_item recent-entry">
-            <img src={assets.setting_icon} alt="" />
-            <p>Setting</p>
-          </div>
+        <div className="bottom_item recent-entry">
+          <img src={assets.setting_icon} alt="" />
+          {extended ? <p>Settings</p> : null}
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default SlidBar
+export default SlidBar;
